3681d78c31afedfd848f54687d7645d515b6bdba
TurtleMakeMods <titus.domey@gmail.com>
2023-03-15 16:28:44 -0700

    added plat ore gen

---
+++ src/main/java/com/devgroup/TerrariaMod/world/feature/ModConfiguredFeatures.java
@@ -0,0 +1,34 @@
+package com.devgroup.TerrariaMod.world.feature;
+
+import com.google.common.base.Suppliers;
+import com.devgroup.TerrariaMod.TerrariaMod;
+import com.devgroup.TerrariaMod.blocks.ModBlocks;
+import net.minecraft.core.Registry;
+import net.minecraft.data.worldgen.features.OreFeatures;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class ModConfiguredFeatures {
+    public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES =
+            DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, TerrariaMod.MOD_ID);
+
+    public static final Supplier<List<OreConfiguration.TargetBlockState>> OVERWORLD_PLATINUM_ORES = Suppliers.memoize(() -> List.of(
+            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.PLATINUM_ORE.get().defaultBlockState()),
+            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.DEEPSLATE_PLATINUM_ORE.get().defaultBlockState())));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> PLATINUM_ORE = CONFIGURED_FEATURES.register("platinum_ore",
+            () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_PLATINUM_ORES.get(), 10)));
+
+
+    public static void register(IEventBus eventBus) {
+        CONFIGURED_FEATURES.register(eventBus);
+    }
+}
